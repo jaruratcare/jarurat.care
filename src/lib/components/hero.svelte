738e978eb7cf2f9f06536da9bb639b7f95e0fc78
rashi jain <rashijain326@gmail.com>
2024-07-19 13:06:34 +0530

    Added who we are

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -23,7 +23,8 @@
       class="flex flex-col items-center gap-4 text-white"
       in:fade={{ duration: 500 }}
       out:fly={{ x: -1000, duration: 1000 }}
-    ></div>
+    >
+    </div>
   {/if}
 
   {#if showFinalScreen}
@@ -51,9 +52,10 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: center; 
     gap: 4rem;
-    padding-top: 10rem;
+    padding: 0;
+    margin: 0; 
     cursor: pointer;
     outline: none;
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import Nav from '$lib/components/nav.svelte';
   import Hero from '$lib/components/hero.svelte';
   import Donation from '$lib/components/donation.svelte';
+  import MissionSection from '$lib/components/mission-section.svelte';
 </script>
 
 <Nav />
 <Hero />
+<MissionSection/>
 <Donation />

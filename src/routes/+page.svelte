deac0eecc627970f9cc08db9fe6de24c509a2905
Md Shami <128491850+shamisaifi@users.noreply.github.com>
2024-07-21 01:50:20 +0530

    Feature/story section (#20)
    * building donation section
    
    * build donation section
    
    * button hover animation
    
    * building donation section
    PS C:\Users\shami\OneDrive\JARAURAT_CARE_website\JCF_new_website\jarurat.care> git rebase origin/develop
    >>
    >>
    Auto-merging src/app.html
    CONFLICT (content): Merge conflict in src/app.html
    Auto-merging src/routes/+page.svelte
    CONFLICT (content): Merge conflict in src/routes/+page.svelte
    error: could not apply 708caf9... building donation section
    building donation section
    
    .git/COMMIT_EDITMSG [unix] (12:58 17/07/2024)                         1,1 Topfiex conflict issue
    :wq
    f ignored, and an empty message aborts the commit.
    
    * build donation section
    
    * button hover animation
    
    * changed the background png to svg
    
    * checked the formatting and errors
    
    * removed google fonts
    
    * fixed issues
    
    * fixed bg size
    
    * Revert "fixed bg size"
    
    This reverts commit c21d4fa4f9ea15f62f86d049d4428435fe8c2da7.
    
    * format and lint
    
    * moved fonts import in +layout.svelte
    
    * fixed slider
    
    * fix: conflicts
    
    ---------
    
    Co-authored-by: Abdus <abdus@abdus.net>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,12 @@
   import Nav from '$lib/components/nav.svelte';
   import Hero from '$lib/components/hero.svelte';
   import Donation from '$lib/components/donation.svelte';
+  import Story from '$lib/components/story.svelte';
 </script>
 
 <Nav />
 <Hero />
 <Donation />
+<Story />
 <VoiceOfHope />
 <HearFromTeam />

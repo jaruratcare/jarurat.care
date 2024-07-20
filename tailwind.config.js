e1a1b20a76ef89fcfa9d040ca0a2e69d7c51646f
rashi jain <106333107+rashijain002@users.noreply.github.com>
2024-07-21 02:01:19 +0530

    Added who we are (#21)
    Co-authored-by: Abdus <abdus@abdus.net>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: { extend: {} },
+  theme: { extend: {
+    colors:{
+      'custom-blue':'#132442',
+    },
+  },
+ },
   // eslint-disable-next-line
   plugins: [require('@tailwindcss/typography')]
 };

8f875f67bf753c6527ac246eb4f0268eb5fd0a02
shamisaifi <shamisaifi2003@gmail.com>
2024-07-21 12:21:41 +0530

    made requested changes

--- src/lib/components/hospitals-location.svelte
+++ src/lib/components/hospitals-location.svelte
@@ -1,57 +1,80 @@
 <script>
+  import { onMount } from 'svelte';
   import LocateIcon from '$lib/svg/locate-icon.svelte';
   import LocationArrow from '$lib/svg/location-arrow.svelte';
   import Map from '$lib/svg/map.svelte';
   import SearchIcon from '$lib/svg/search-icon.svelte';
 
-const statesAndUnionTerritories = [
-  "Andhra Pradesh",
-  "Arunachal Pradesh",
-  "Assam",
-  "Bihar",
-  "Chhattisgarh",
-  "Goa",
-  "Gujarat",
-  "Haryana",
-  "Himachal Pradesh",
-  "Jharkhand",
-  "Karnataka",
-  "Kerala",
-  "Madhya Pradesh",
-  "Maharashtra",
-  "Manipur",
-  "Meghalaya",
-  "Mizoram",
-  "Nagaland",
-  "Odisha",
-  "Punjab",
-  "Rajasthan",
-  "Sikkim",
-  "Tamil Nadu",
-  "Telangana",
-  "Tripura",
-  "Uttar Pradesh",
-  "Uttarakhand",
-  "West Bengal",
-  "Andaman and Nicobar Islands",
-  "Chandigarh",
-  "Dadra and Nagar Haveli and Daman and Diu",
-  "Delhi",
-  "Lakshadweep",
-  "Puducherry",
-  "Ladakh",
-  "Jammu and Kashmir"
-];
+  const statesAndUnionTerritories = [
+    { id: 'AP', state: 'Andhra Pradesh' },
+    { id: 'AR', state: 'Arunachal Pradesh' },
+    { id: 'AS', state: 'Assam' },
+    { id: 'BR', state: 'Bihar' },
+    { id: 'CG', state: 'Chhattisgarh' },
+    { id: 'GA', state: 'Goa' },
+    { id: 'GJ', state: 'Gujarat' },
+    { id: 'HR', state: 'Haryana' },
+    { id: 'HP', state: 'Himachal Pradesh' },
+    { id: 'JH', state: 'Jharkhand' },
+    { id: 'KA', state: 'Karnataka' },
+    { id: 'KL', state: 'Kerala' },
+    { id: 'MP', state: 'Madhya Pradesh' },
+    { id: 'MH', state: 'Maharashtra' },
+    { id: 'MN', state: 'Manipur' },
+    { id: 'ML', state: 'Meghalaya' },
+    { id: 'MZ', state: 'Mizoram' },
+    { id: 'NL', state: 'Nagaland' },
+    { id: 'OD', state: 'Odisha' },
+    { id: 'PB', state: 'Punjab' },
+    { id: 'RJ', state: 'Rajasthan' },
+    { id: 'SK', state: 'Sikkim' },
+    { id: 'TN', state: 'Tamil Nadu' },
+    { id: 'TS', state: 'Telangana' },
+    { id: 'TR', state: 'Tripura' },
+    { id: 'UP', state: 'Uttar Pradesh' },
+    { id: 'UK', state: 'Uttarakhand' },
+    { id: 'WB', state: 'West Bengal' },
+    { id: 'AN', state: 'Andaman and Nicobar Islands' },
+    { id: 'CH', state: 'Chandigarh' },
+    { id: 'DH', state: 'Dadra and Nagar Haveli and Daman and Diu' },
+    { id: 'DL', state: 'Delhi' },
+    { id: 'LD', state: 'Lakshadweep' },
+    { id: 'PY', state: 'Puducherry' },
+    { id: 'LA', state: 'Ladakh' },
+    { id: 'JK', state: 'Jammu and Kashmir' }
+  ];
 
+  let stateName = "Gujarat"
 
-  const getStateData = (event) => {
-    const ids = event.target.id;
-    statesAndUnionTerritories.map((state) => {
-      if (state === ids) {
-        console.log(state);
-      }
+  const state = 'state';
+  onMount(() => {
+    const states = document.querySelectorAll('.state');
+    states.forEach((state) => {
+      state.addEventListener('click', (ev) => {
+        const id = ev.target.id;
+        // console.log('click', id);
+        statesAndUnionTerritories.forEach((state) => {
+          if (state.id === id) {
+            console.log('id:', id, ', state:', state.state);
+            stateName = state.state
+          }
+        });
+      });
     });
-  };
+
+    return () => {
+      states.forEach((state) => {
+        state.removeEventListener('click', (ev) => {
+          const id = ev.target.id;
+          statesAndUnionTerritories.forEach((state) => {
+            if (state?.id === id) {
+              console.log(state.state);
+            }
+          });
+        });
+      });
+    };
+  });
 </script>
 
 <div class="w-full py-48">
@@ -81,51 +104,64 @@ const statesAndUnionTerritories = [
       <div class="w-[25rem] mx-2 flex items-center bg-[#0155BD] rounded-full p-2">
         <select
           class="py-[.3rem] px-7 bg-[#0155BD] border-none outline-none text-white appearance-none w-full"
-          
         >
-          <option value="" selected disabled class="">Select your state</option>
+          <option value="" selected disabled>Select your state</option>
           {#each statesAndUnionTerritories as state}
-            <option value={state} id={state}>{state}</option>
+            <option value={state?.state} id={state?.id} class="state">{state?.state}</option>
           {/each}
         </select>
       </div>
     </div>
 
     <div class=" w-full h-full mt-32 flex">
-      <div class="w-1/2  flex justify-center ml-20 mb-2">
+      <div class="w-1/2 flex justify-center ml-20 mb-2">
         <div class="w-3/5">
           <div class="w-full">
-            <p class="text-sm text-[#0155BD] bg-[#e3efff] py-1 px-3 border-l-2  border-[#0155BD] rounded-r-md ">
-              Note: we might be missing some hospitals or maybe they are unregistrered
+            <p
+              class="text-sm text-[#0155BD] bg-[#e3efff] py-1 px-3 border-l-2 border-[#0155BD] rounded-r-md"
+            >
+              Note: we might be missing some hospitals or maybe they are unregistered
             </p>
           </div>
           <div class=" mt-5 rounded-t-md overflow-hidden w-full">
-            <p class="bg-[#0D2561] text-white p-2 text-2xl font-semibold " style="font-family: 'Roboto', sans-serif;" >Gujrat</p>
-            <ul class="w-full ">
-              <li class="py-2 px-4 w-full ">
+            <p
+              class="bg-[#0D2561] text-white p-2 text-2xl font-semibold"
+              style="font-family: 'Roboto', sans-serif;"
+            >
+              {stateName}
+            </p>
+            <ul class="w-full">
+              <li class="py-2 px-4 w-full">
                 <p>Wockhardt Hospital</p>
-                <p class="text-[#868B93] flex justify-between ">Kalawad Road Near St. Mary's High Scho... <LocationArrow/></p>
+                <p class="text-[#868B93] flex justify-between">
+                  Kalawad Road Near St. Mary's High Scho... <LocationArrow />
+                </p>
               </li>
-              <li class="py-2 px-4 w-full ">
+              <li class="py-2 px-4 w-full">
                 <p>Wockhardt Hospital</p>
-                <p class="text-[#868B93] flex justify-between ">Kalawad Road Near St. Mary's High Scho... <LocationArrow/></p>
+                <p class="text-[#868B93] flex justify-between">
+                  Kalawad Road Near St. Mary's High Scho... <LocationArrow />
+                </p>
               </li>
-              <li class="py-2 px-4 w-full ">
+              <li class="py-2 px-4 w-full">
                 <p>Wockhardt Hospital</p>
-                <p class="text-[#868B93] flex justify-between ">Kalawad Road Near St. Mary's High Scho... <LocationArrow/></p>
+                <p class="text-[#868B93] flex justify-between">
+                  Kalawad Road Near St. Mary's High Scho... <LocationArrow />
+                </p>
               </li>
-              <li class="py-2 px-4 w-full ">
+              <li class="py-2 px-4 w-full">
                 <p>Wockhardt Hospital</p>
-                <p class="text-[#868B93] flex justify-between ">Kalawad Road Near St. Mary's High Scho... <LocationArrow/></p>
+                <p class="text-[#868B93] flex justify-between">
+                  Kalawad Road Near St. Mary's High Scho... <LocationArrow />
+                </p>
               </li>
-              
             </ul>
           </div>
         </div>
       </div>
 
-      <div class="w-1/2 mr-12 mb-2 flex justify-center ">
-        <Map {getStateData} />
+      <div class="w-1/2 mr-12 mb-2 flex justify-center">
+        <Map {state} />
       </div>
     </div>
   </div>

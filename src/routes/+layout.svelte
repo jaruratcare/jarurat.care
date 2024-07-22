ace4f9b11950b710ee670acfe6f18d3ca2ae35f1
Ravitejapatnala <128287646+Ravitejapatnala@users.noreply.github.com>
2024-07-23 04:29:36 +0530

    Added styles using tailwind CSS (#29)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
+  import Footer from '$lib/components/Footer/footer.svelte';
   import '../app.css';
   import '@fontsource/roboto/500.css';
   import '@fontsource/roboto/100.css';
   import '@fontsource/public-sans/500.css';
 </script>
 
-<slot />
+<main>
+  <slot />
+  <Footer/>
+</main>
+

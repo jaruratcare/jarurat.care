be80da004782278a02aae41fcb2fc8c58ff6838b
shamisaifi <shamisaifi2003@gmail.com>
2024-07-23 19:33:43 +0530

    build FAQs section

---
+++ src/lib/svg/FAQ-gradient.svelte
@@ -0,0 +1,125 @@
+<svg
+  width="100%"
+  height="100%"
+  viewBox="0 0 400 480"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g filter="url(#filter0_f_193_2376)">
+    <rect
+      x="402"
+      y="579"
+      width="208"
+      height="178"
+      transform="rotate(-180 402 579)"
+      fill="#A395F8"
+    />
+  </g>
+  <g filter="url(#filter1_f_193_2376)">
+    <rect
+      x="205"
+      y="558"
+      width="208"
+      height="178"
+      transform="rotate(-180 205 558)"
+      fill="#00C8F4"
+    />
+  </g>
+  <g filter="url(#filter2_f_193_2376)">
+    <rect
+      x="124"
+      y="283"
+      width="208"
+      height="178"
+      transform="rotate(-180 124 283)"
+      fill="#81F5FF"
+    />
+  </g>
+  <g filter="url(#filter3_f_193_2376)">
+    <rect
+      x="356"
+      y="417"
+      width="208"
+      height="178"
+      transform="rotate(-180 356 417)"
+      fill="#30A8FF"
+    />
+  </g>
+  <g filter="url(#filter4_f_193_2376)">
+    <rect
+      x="413"
+      y="215"
+      width="208"
+      height="178"
+      transform="rotate(-180 413 215)"
+      fill="#9747FF"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_f_193_2376"
+      x="14"
+      y="221"
+      width="568"
+      height="538"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+      <feGaussianBlur stdDeviation="90" result="effect1_foregroundBlur_193_2376" />
+    </filter>
+    <filter
+      id="filter1_f_193_2376"
+      x="-183"
+      y="200"
+      width="568"
+      height="538"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+      <feGaussianBlur stdDeviation="90" result="effect1_foregroundBlur_193_2376" />
+    </filter>
+    <filter
+      id="filter2_f_193_2376"
+      x="-264"
+      y="-75"
+      width="568"
+      height="538"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+      <feGaussianBlur stdDeviation="90" result="effect1_foregroundBlur_193_2376" />
+    </filter>
+    <filter
+      id="filter3_f_193_2376"
+      x="-32"
+      y="59"
+      width="568"
+      height="538"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+      <feGaussianBlur stdDeviation="90" result="effect1_foregroundBlur_193_2376" />
+    </filter>
+    <filter
+      id="filter4_f_193_2376"
+      x="25"
+      y="-143"
+      width="568"
+      height="538"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+      <feGaussianBlur stdDeviation="90" result="effect1_foregroundBlur_193_2376" />
+    </filter>
+  </defs>
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script>
-  import HearFromTeam from "$lib/components/hear-from-team.svelte";
-  import VoiceOfHope from "$lib/components/VoiceOfHope.svelte";
+  import HearFromTeam from '$lib/components/hear-from-team.svelte';
+  import VoiceOfHope from '$lib/components/VoiceOfHope.svelte';
   import Nav from '$lib/components/nav.svelte';
   import Hero from '$lib/components/hero.svelte';
   import Donation from '$lib/components/donation.svelte';
   import MissionSection from '$lib/components/mission-section.svelte';
   import Story from '$lib/components/story.svelte';
+  import FaqSection from '$lib/components/FAQ-section.svelte';
 </script>
 
 <Nav />
 <Hero />
-<MissionSection/>
+<MissionSection />
 <Donation />
 <Story />
 <VoiceOfHope />
 <HearFromTeam />
+<FaqSection />

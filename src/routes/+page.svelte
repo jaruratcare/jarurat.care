46db5292a893656bbc93e9a1ee2591a4387567c9
rashi jain <106333107+rashijain002@users.noreply.github.com>
2024-07-24 21:40:49 +0530

    "closes #5" "How we support you" section  (#27)
    * "How we support you" section
    
    * "How we support you" section made requested changes
    
    * Made requested changes
    
    ---------
    
    Co-authored-by: Abdus <abdus@abdus.net>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
   import Hero from '$lib/components/hero.svelte';
   import Donation from '$lib/components/donation.svelte';
   import Story from '$lib/components/story.svelte';
+  import SupportSection from '$lib/components/support-section.svelte';
+  import FindMentor from '$lib/components/find-mentor.svelte';
   import FaqSection from '$lib/components/FAQ-section.svelte';
   import HospitalsLocation from '$lib/components/hospitals-location.svelte';
   import MissionSection from '$lib/components/mission-section.svelte';
@@ -15,6 +17,8 @@
 <MissionSection />
 <Donation />
 <Story />
+<SupportSection/>
+<FindMentor/>
 <HospitalsLocation />
 <VoiceOfHope />
 <HearFromTeam />
